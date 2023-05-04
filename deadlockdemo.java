5d96741611b86630d282a7e05bdb5fcf134d45fd
AniketHait <42712707+AniketHait@users.noreply.github.com>
2023-05-04 18:00:54 +0530

    Add files via upload

---
+++ deadlockdemo.java
@@ -0,0 +1,29 @@
+package ThreadsPractice;
+class DataInitializer {
+}
+public class deadlockdemo {
+    static int initialVal;
+    public static void increase() {
+        initialVal = initialVal+1;
+    }
+    public static void main(String[] args) throws InterruptedException {
+    Runnable ch1 = () -> {
+        for(int i =0;i<=100;i++) {
+           increase();
+        }
+    };
+    Runnable ch2 = () -> {
+        for(int i =0;i<=100;i++) {
+            increase();
+        }
+    };
+    Thread t1 = new Thread(ch1);
+    Thread t2 = new Thread(ch2);
+    //Object 
+    t1.start();
+    t2.start();
+    t1.join();
+    t2.join();
+    System.out.println(initialVal);
+}
+}
